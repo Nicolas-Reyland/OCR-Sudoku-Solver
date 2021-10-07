11c7e6101f84d3b849813cdf3db5fd1874e22b7d
Nicolas Reyland <nicolas.reyland@gmail.com>
2021-10-08 00:06:06 +0200

    added misc dir w/ randomness src files

---
+++ src/nn/utils/misc/randomness.c
@@ -0,0 +1,38 @@
+// randomness.c
+
+#include "randomness.h"
+
+/* Initialize randomness
+ */
+void nn_initRandom()
+{
+  if (_nn_random_init_done)
+    return;
+  srand( time( NULL ));
+  _nn_random_init_done = true;
+}
+
+/* Returns a random double value between min_value and max_value
+ */
+double getRandomDouble(double min_value, double max_value)
+{
+  double range = max_value - min_value;
+  double random_double = (double) rand() / (D_RAND_MAX / range);
+  return random_double + min_value;
+}
+
+/* Returns a random double value between 0.0 and 1.0
+ */
+double getNormalizedPositiveRandomDouble()
+{
+  return (double) rand() / D_RAND_MAX;
+}
+
+/* Returns a random double value between -1.0 and 1.0
+ */
+double getNormalizedRandomDouble()
+{
+  return (double) rand() / RAND_MAX_DIV_BY_2 - 1.0;
+}
+
+//
---
+++ src/nn/utils/misc/randomness.h
@@ -0,0 +1,19 @@
+// randomness.h
+
+#ifndef NN_RANDOMNESS_H
+#define NN_RANDOMNESS_H
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+static bool _nn_random_init_done = false;
+static const double D_RAND_MAX = RAND_MAX;
+static const double RAND_MAX_DIV_BY_2 = RAND_MAX / 2;
+
+void _nn_initRandom();
+double getRandomDouble(double min_value, double max_value);
+double getNormalizedPositiveRandomDouble();
+double getNormalizedRandomDouble();
+
+#endif
