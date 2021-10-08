10f8822690edd910008b55ed52c126f6a8164abd
Lilian Schall <lilian.schall@epita.fr>
2021-10-08 21:06:03 +0200

    added init_data file, which will contain functions that construct nn_Data based on a input file

---
+++ src/nn/data/init_data.c
@@ -0,0 +1,7 @@
+#include "init_data.h"
+#include <stdio.h>
+
+nn_Data* nn_Data_load_raw(char* path)
+{
+
+}
---
+++ src/nn/data/init_data.h
@@ -0,0 +1,14 @@
+//init_data.h
+
+#ifndef INIT_DATA_H
+#define INIT_DATA_H
+
+#include "data.h"
+#include "in_out_tuple.h"
+
+//public functions
+nn_Data* nn_Data_load_raw(char* path);
+
+//private functions
+
+#endif
