fd9279c54763e12bb3e024e8fb4dc20ab33dadd4
Lilian Schall <lilian.schall@epita.fr>
2021-10-08 22:47:42 +0200

    Made some advances in init_data, currently working on reading input_file based on shape description, so that we can build a data object that will be fed to the model.

--- src/nn/data/init_data.c
+++ src/nn/data/init_data.c
@@ -1,15 +1,63 @@
 #include "init_data.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-nn_Data* nn_Data_load_raw(char* path)
+double _convertStringToDouble(char* string);
+
+
+
+nn_Data* nn_Data_load_raw(char* path, nn_ShapeDescription description)
 {
     FILE* input_file = fopen(path,"r+");
 
     if(input_file == NULL)
     {
-        printf("%s, the file does not exist. Exiting...", &path);
+        printf("%s, the file does not exist. Exiting...\n", &path);
         exit(EXIT_FAILURE);
     }
     
+    char car = fgetc(input_file);
+    size_t j = 0; //cursor in our double array;
+
+    //the size of the double array
+    size_t value_size = description.x*description.y*description.z; 
+
+    //normally, if we do the right things, then we define the dimensions
+    //that are not used to 1, so that it doesn't break the malloc sizing lol
+    double* value = malloc(sizeof(double)*value_size); 
+    while(car != EOF)
+    {
+        char* string = malloc(sizeof(char) * NB_DOUBLE_BITS);
+        size_t i = 0; //cursor in our string
+
+        
+        
+        if(car != ' ')
+        {
+            //if we have allocated enough space to the string
+            if(i < NB_DOUBLE_BITS)  
+                string[i] = car;
+            else                    //dear God
+            {
+                printf("Error at loading data, string buffer not enough big to\
+                 store current data... exiting\n");
+                exit(EXIT_FAILURE);
+            }
+            i++;
+        }
+        else //if we hit a space, than we convert the string
+        // and store the value in the array
+        {
+            value[j] = _convertStringToDouble(string);
+        }
+        car = fgetc(input_file);
+    }
+    
+}
+
+double _convertStringToDouble(char* string)
+{
+    //more on the next episode
+    return 0;
 }
--- src/nn/data/init_data.h
+++ src/nn/data/init_data.h
@@ -3,11 +3,13 @@
 #ifndef INIT_DATA_H
 #define INIT_DATA_H
 
+#define NB_DOUBLE_BITS 64
+
 #include "data.h"
 #include "in_out_tuple.h"
 
 //public functions
-nn_Data* nn_Data_load_raw(char* path);
+nn_Data* nn_Data_load_raw(char* path, nn_ShapeDescription nn_ShapeDescription);
 
 //private functions
 
