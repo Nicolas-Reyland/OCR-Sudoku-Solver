68ad7ce0a987a0fcdee4f5ad18d978f54db394f2
Nicolas Reyland <nicolas.reyland@gmail.com>
2021-10-10 16:00:41 +0200

    fixed data.h/c errors

--- src/nn/data/data.h
+++ src/nn/data/data.h
@@ -4,15 +4,21 @@
 #define NN_DATA_H
 
 #include "data_collection.h"
-#include "data_tuple.h"
-// code comes here
+
+typedef struct nn_DataTuple nn_DataTuple;
+
 typedef struct nn_Data //needs to be defined as a pointer please
 {
     nn_DataCollection* data_collection;
 
-    nn_DataTuple (*splitTrainTest) (struct nn_Data* data,
-     int splittingPercentage);
+    nn_DataTuple (*splitTrainTest) (struct nn_Data*, int);
 } nn_Data;
+
+typedef struct nn_DataTuple
+{
+	nn_Data* data1;
+	nn_Data* data2;
+} nn_DataTuple;
 
 nn_Data* createData(nn_DataCollection* collection);
 void freeData(nn_Data* data);
