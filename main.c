42354021e0b131b62799c92299093f0136f814c8
Nicolas Reyland <nicolas.reyland@gmail.com>
2021-10-11 17:44:17 +0200

    better includes in main.c

--- main.c
+++ main.c
@@ -3,24 +3,25 @@
 #include "src/nn/model/model.h"
 #include "src/nn/model/model.h"
 #include "src/nn/utils/misc/randomness.h"
+#include "src/nn/functions_descriptors/functions_descriptors_enums.h"
 
 int main()
 {
 	// init random
 	_nn_initRandom();
 	// model architecture
-	ShapeDescription model_architecture[3] = {
+	nn_ShapeDescription model_architecture[3] = {
 		{ .dims = 3, .x = 3, .y = 3, .z = 3 },
 		{ .dims = 2, .x = 5, .y = 2, .z = 1 },
 		{ .dims = 1, .x = 1, .y = 1, .z = 1 }
 	};
 	// activation functions
-	activationFunction activations[3] = {
+	activation activations[3] = {
 		0, 1, 2
 	};
 	// loss & optimizers
-	lossFunction loss = 0;
-	optimizer optimizer = 0;
+	losses loss = CATEGORICALCROSSENTROPY;
+	optimizer optimizer = ADAM;
 	// malloc model
 	nn_Model* model = createModel(3, model_architecture, activations, loss, optimizer);
 
