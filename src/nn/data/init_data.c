8b7f54146814de6c8879309b11d939536663e912
Lilian Schall <lilian.schall@epita.fr>
2021-10-11 14:32:00 +0200

    resolving push conflict

--- src/nn/data/init_data.c
+++ src/nn/data/init_data.c
@@ -1,5 +1,3 @@
-// init_data.c
-
 #include "init_data.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,17 +6,11 @@
 double _convertStringToDouble(char* string);
 char _readFile(FILE* file, nn_ShapeDescription* description, double* values);
 
-<<<<<<< HEAD
 
 nn_Data* nn_Data_load_raw(char* input_path, char* output_path, nn_ShapeDescription* description)
 {
     FILE* input_file = fopen(input_path,"r+");
     FILE* output_file = fopen(output_path, "r+");
-=======
-nn_Data* nn_Data_load_raw(char* path, nn_ShapeDescription* description)
-{
-    FILE* input_file = fopen(path, "r+");
->>>>>>> 4e1ff95e62ae59a4a0b2f6d657a9256ab9f7d38b
 
     if(input_file == NULL)
     {
@@ -30,7 +22,6 @@ nn_Data* nn_Data_load_raw(char* path, nn_ShapeDescription* description)
         printf("ERROR 404: %s, the file does not exist. Exiting...\n", *output_path);
         exit(EXIT_FAILURE);
     }
-<<<<<<< HEAD
     linked_list* data_list = init_linked_list();
 
 
@@ -48,11 +39,6 @@ nn_Data* nn_Data_load_raw(char* path, nn_ShapeDescription* description)
         double* input_values;
         double* output_values;
         cursorInput = readFile(input_file,description, input_values);
-=======
-
-    fscanf(input_file,"%d %d %d %d %d",&description->dims, &description->x,
-    &description->y, &description->z);
->>>>>>> 4e1ff95e62ae59a4a0b2f6d657a9256ab9f7d38b
 
         //we don't need to read the cursor of output_file
         //since normally it should have the same nb of lines than
@@ -62,13 +48,7 @@ nn_Data* nn_Data_load_raw(char* path, nn_ShapeDescription* description)
         nn_Sample* input    = createSample(*description,output_values);
         nn_Sample* output   = createSample(*description,input_values);
 
-<<<<<<< HEAD
         nn_InOutTuple* tuple = createInOutTuple(input,output);
-=======
-    char car = fgetc(input_file);
-    while(car != '\n')
-        car = fgetc(input_file); //reading line to end
->>>>>>> 4e1ff95e62ae59a4a0b2f6d657a9256ab9f7d38b
 
         data_list->append_value(data_list,tuple);
     } while (cursorInput != EOF);
@@ -86,29 +66,24 @@ char _readFile(FILE* file, nn_ShapeDescription* description, double* values)
     size_t j = 0; //cursor in our double array;
 
     //the size of the double array
-    size_t value_size = description->x*description->y*description->z;
+    size_t value_size = description->x*description->y*description->z; 
 
     //normally, if we do the right things, then we define the dimensions
     //that are not used to 1, so that it doesn't break the malloc sizing lol
-<<<<<<< HEAD
     values = malloc(sizeof(double)*value_size); 
 
     char car = fgetc(file);
     while(car != '\n' && car != EOF)
-=======
-    double* value = malloc(sizeof(double)*value_size);
-    while(car != EOF)
->>>>>>> 4e1ff95e62ae59a4a0b2f6d657a9256ab9f7d38b
     {
         char* string = malloc(sizeof(char) * NB_DOUBLE_BITS);
         size_t i = 0; //cursor in our string
 
-
-
+        
+        
         if(car != ' ')
         {
             //if we have allocated enough space to the string
-            if(i < NB_DOUBLE_BITS)
+            if(i < NB_DOUBLE_BITS)  
                 string[i] = car;
             else                    //dear God
             {
@@ -125,13 +100,7 @@ char _readFile(FILE* file, nn_ShapeDescription* description, double* values)
         }
         car = fgetc(file);
     }
-<<<<<<< HEAD
     return car;
-=======
-    fclose(input_file);
-
-    // return something here
->>>>>>> 4e1ff95e62ae59a4a0b2f6d657a9256ab9f7d38b
 }
 
 /// <Summary>
