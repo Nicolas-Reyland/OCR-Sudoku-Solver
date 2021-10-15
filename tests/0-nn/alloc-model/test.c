01888bf71d614e6453f95d3e3eb22724e5721439
Nicolas Reyland <nicolas@reyland.dev>
2021-10-15 20:18:42 +0200

    moved test.c file

--- tests/0-nn/alloc-model/test.c
+++ tests/0-nn/alloc-model/test.c
@@ -1,9 +1,14 @@
 #include "utils/verbosity/verbose.h"
 #include "nn/nn.h"
 #include "utils/mem/mem-management.h"
+#include "utils/verbosity/verbose.h"
 
-int main()
+int main(int argc, char** argv)
 {
+	if (argc > 1 && argv[1][0]) {
+		setVerbose(false);
+	}
+
 	// init random
 	initRandom();
 	initMemoryTracking();
@@ -21,15 +26,17 @@ int main()
 	losses loss = CATEGORICALCROSSENTROPY;
 	optimizer optimizer = ADAM;
 
-	verbose("Allocating model...\n");
+	verbose("Allocating model...");
 
 	// malloc model
 	nn_Model* model = createModel(3, model_architecture, activations, loss, optimizer);
 
-	verbose("Model allocated.\n");
+	verbose("Model allocated.");
 
 	// free model
 	freeModel(model);
+
+	verbose("Model freed.");
 
 	return 0;
 }
