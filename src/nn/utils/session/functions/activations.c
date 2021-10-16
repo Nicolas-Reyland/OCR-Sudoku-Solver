00780ed7bc1f9d64be2b59987516bcd12d635ea1
Lilian.schall <lilian.schall@epita.fr>
2021-10-16 18:30:06 +0200

    added softmax activation function

--- src/nn/utils/session/functions/activations.c
+++ src/nn/utils/session/functions/activations.c
@@ -1,6 +1,9 @@
 // activations.c
 
 #include "activations.h"
+double _nn_sigmoid(double x);
+double _nn_relu(double x);
+double _nn_softmax(double sum, double x);
 
 void _nn_activateLayer(nn_Layer* layer)
 {
@@ -12,7 +15,8 @@ void _nn_activateLayer(nn_Layer* layer)
 		case RELU:
 			relu(layer);
 			break;
-		// case ...
+		case SOFTMAX!
+			softmax(layer);
 		default:
 			fprintf(stderr, "Unrecognised activation function: %d\n", layer->activation);
 			exit(EXIT_FAILURE);
@@ -44,4 +48,18 @@ void relu(nn_Layer* layer)
 		layer->nodes[i]->value = _nn_relu(layer->nodes[i]->raw_value);
 }
 
+double _nn_softmax(double sum, double x)
+{
+	return sum / x;
+}
 
+/* SoftMAX function applied to whole layer*/
+void softmax(nn_Layer* layer)
+{
+	double sum =0;
+	for(size_t i = 0; i < layer->nb_nodes;i++)
+		sum += exp(layer->nodes[i]->raw_value);
+	
+	for(size_t i = 0; i< layer->nb_nodes;i++)
+		layer->nodes[i]->value = _nn_softmax(sum, layer->nodes[i]->raw_value);
+}
