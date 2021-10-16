c28100582bc91b62d1894d9bd94038ed63d62774
Nicolas Reyland <nicolas@reyland.dev>
2021-10-16 14:19:43 +0200

    fixed function arg type

--- src/nn/utils/session/functions/losses.h
+++ src/nn/utils/session/functions/losses.h
@@ -3,6 +3,6 @@
 #ifndef NN_LOSSES_H
 #define NN_LOSSES_H
 
-void _nn_emptyLossFunction(nn_ModelLayers* model_layers, double* desired_output);
+void _nn_emptyLossFunction(nn_Layer* model_layers, double* desired_output);
 
 #endif
