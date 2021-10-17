2b3d87e6d31017bb05a00b2939469780af218247
Lilian.schall <lilian.schall@epita.fr>
2021-10-17 06:54:23 +0200

    added new loss Function Meansquarederror

--- src/nn/utils/session/functions/losses.c
+++ src/nn/utils/session/functions/losses.c
@@ -4,6 +4,7 @@
 
 double _nn_binaryCrossEntropy(nn_Layer* layer, double* desired_output);
 double _nn_categoricalCrossEntropy(nn_Layer* layer, double* desired_output);
+double _nn_meanSquaredError(nn_Layer* layer, double* desired_output);
 
 double applyLosses(nn_Layer* layer, double *desired_output, losses losses)
 {
@@ -14,6 +15,8 @@ double applyLosses(nn_Layer* layer, double *desired_output, losses losses)
 			break;
 		case BINARYCROSSENTROPY:
 			return _nn_binaryCrossEntropy(layer, desired_output);
+		case MEANSQUAREDERROR:
+			return _nn_meanSquaredError(layer,desired_output);
 		default:
 			fprintf(stderr, "ApplyLosses: unrecognized losses function: %d\n",
 			 losses);
@@ -30,7 +33,7 @@ double _nn_binaryCrossEntropy(nn_Layer* layer, double* desired_output)
 	for(size_t i = 0; i < layer->nb_nodes; i++)
 	{
 		sum += desired_output[i] * log(layer->nodes[i]->value) + 
-		(1 - desired_output[i])* log(1 - layer->nodes[i]->value);
+		(1.0 - desired_output[i])* log(1 - layer->nodes[i]->value);
 	}
 	sum = sum * (1/layer->nb_nodes);
 	return - sum;
@@ -46,6 +49,17 @@ double _nn_categoricalCrossEntropy(nn_Layer* layer, double* desired_output)
 		sum += desired_output[i] * log(layer->nodes[i]->value);
 	}
 	return -sum;
+}
+
+double _nn_meanSquaredError(nn_Layer* layer, double* desired_output)
+{
+	double sum = 0;
+	for(size_t i = 0; i < layer->nb_nodes; i++)
+	{
+		sum += (desired_output[i] - layer->nodes[i]->value) * (desired_output[i] - layer->nodes[i]->value);  
+	}
+	double mean_square_error = (1.0 / layer->nb_nodes) * sum;
+	return mean_square_error;
 }
 
 
