b8c1de5b9c4bc0541a603b7d62008ec1af7b92c4
Lilian.schall <lilian.schall@epita.fr>
2021-10-17 07:34:55 +0200

    added test function to session struct

--- src/nn/session/session.c
+++ src/nn/session/session.c
@@ -13,25 +13,33 @@ void _nn_train(struct nn_Session* session, nn_Model* model)
 	size_t epoch = 0;
 	while (epoch < session->nb_epochs && loss_threshold_condition)
 	{
-		verbose("Epoch number: %ld",epoch);
-		verbose("Shuffling tuples...");
+		if(session->verbose)
+		{
+			verbose("Epoch number: %ld",epoch);
+			verbose("Shuffling tuples...");
+		}
 		shuffleArray(tuple_array,sample_size);
 		size_t i = 0;
 		while(i < sample_size && loss_threshold_condition)
 		{
-			verbose("Training Tuple:");
-			tuple_array[i]->printTuple(tuple_array[i]);
+			if(session->verbose)
+			{
+				verbose("Testing Tuple:");
+				tuple_array[i]->printTuple(tuple_array[i]);			
+			}
 			_nn_feedForward(model->layers,tuple_array[i]->input->values);
+
 			//we calculate the loss function 
 			double error = applyLosses(
 			&(model->layers->output_layer),
 			tuple_array[i]->output->values,
-			model->loss
-			);
-			verbose("Losses error = %f",error);
+			model->loss);
+			if(session->verbose)
+				verbose("Losses error = %f",error);
+
 			if(session->verbose)
 			{
-				//model->layers->printModelLayers(model->layers);
+				model->layers->printModelLayers(model->layers);
 				model->layers->printModelLayersValues(model->layers);
 			}
 			// we continue as long as we do not reached loss threshold 
@@ -52,8 +60,29 @@ void _nn_train(struct nn_Session* session, nn_Model* model)
 
 void _nn_test(struct nn_Session* session,nn_Model* model)
 {
-	//code goes here
-	return;
+	nn_InOutTuple** tuple_array = iot_linked_list_to_array(
+		session->dataset->test->data_collection->data);
+	size_t sample_size =session->dataset->test->data_collection->data->length;
+
+	shuffleArray(tuple_array,sample_size);
+	for(size_t i = 0; i < sample_size; i++)
+	{
+		verbose("Training Tuple:");
+		tuple_array[i]->printTuple(tuple_array[i]);
+		_nn_feedForward(model->layers,tuple_array[i]->input->values);
+		
+		verbose("Result:");
+		for(size_t j = 0; j < model->layers->output_layer.nb_nodes; j++)
+			verbose_no_endline("%f ",
+			model->layers->output_layer.nodes[j]->value);
+		verbose("");
+		//we calculate the loss function 
+		double error = applyLosses(
+		&(model->layers->output_layer),
+		tuple_array[i]->output->values,
+		model->loss);
+		verbose("Losses error = %f",error);
+	}
 }
 
 nn_Session* createSession(nn_DataSet* dataset, unsigned int nb_epochs,
