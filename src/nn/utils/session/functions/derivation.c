374d3b177e643043b1af40debaf2224262d93f0e
Nicolas Reyland <nicolas@reyland.dev>
2021-10-20 20:49:58 +0200

    fixed _nn_derivativeActivation function structure (indentation & flow control)

--- src/nn/utils/session/functions/derivation.c
+++ src/nn/utils/session/functions/derivation.c
@@ -8,25 +8,22 @@ double dRelu(double x);
 /* Derivative Activation */
 double _nn_derivativeActivation(double x, activation activation)
 {
-    double output;
+  double output;
 	switch(activation)
-    {
-        case SIGMOID:
-            output = dSigmoid(x);
-            break;
-        case RELU:
-            output = dRelu(x);
-            break;
-        case SOFTMAX:
-            output = dSigmoid(x); //there is no derivative for softmax
-            break;
-        default:
-            fprintf(stderr, 
-            "derivativeActivation: Unrecognised activation function: %d\n",
-             activation);
-			exit(EXIT_FAILURE);
-			break;
-    }
+  {
+      case SIGMOID:
+          return dSigmoid(x);
+      case RELU:
+          return dRelu(x);
+      case SOFTMAX:
+          return dSigmoid(x); //there is no derivative for softmax
+      default:
+          fprintf(stderr, 
+          "derivativeActivation: Unrecognised activation function: %d\n",
+           activation);
+			     exit(EXIT_FAILURE);
+		       break;
+  }
 	return output;
 }
 
