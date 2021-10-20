f2a7f76fc297405a07b611286d0ff6b81844a709
Nicolas Reyland <nicolas@reyland.dev>
2021-10-20 20:30:47 +0200

    fixed parts of backpropagation

--- src/nn/utils/session/evaluate.c
+++ src/nn/utils/session/evaluate.c
@@ -43,19 +43,23 @@ void _nn_backPropagation(nn_Model* model, double* desired_output)
   
   // get model layers struct
   nn_ModelLayers* model_layers = model->layers;
+  
+  // look for layer before output (hidden or input)
+  nn_Layer* layer_before_output;
+
+  if (model_layers->num_hidden_layers) {
+    layer_before_output = &model_layers->hidden_layers[model_layers->num_hidden_layers - 1];
+  } else {
+    layer_before_output = &model_layers->input_layer;
+  }
+
   // Calculate output layer derivative weights, values etc.
   nn_Layer* output_layer = &model_layers->output_layer;
   for (size_t j = 0; j < output_layer->nb_nodes; j++) {
     // calculate derivative raw_value of output_layer
-    output_layer->nodes[j]->delta_value = (output_layer->nodes[j]->value - desired_output[j]) * _nn_derivativeActivation(output_layer->nodes[j]->raw_value, output_layer->activation);
-    // derivative weights of output_layer
-    nn_Layer* layer_before_output;
+    double d_error = output_layer->nodes[j]->value - desired_output[j];
+    output_layer->nodes[j]->delta_value = d_error * _nn_derivativeActivation(output_layer->nodes[j]->value, output_layer->activation);
 
-    if (model_layers->num_hidden_layers) {
-      layer_before_output = &model_layers->hidden_layers[model_layers->num_hidden_layers - 1];
-    } else {
-      layer_before_output = &model_layers->input_layer;
-    }
     for (size_t k = 0; k < layer_before_output->nb_nodes; k++) {
       layer_before_output->nodes[k]->d_weights[j] = output_layer->nodes[j]->delta_value * layer_before_output->nodes[k]->value;
       layer_before_output->nodes[k]->d_value = layer_before_output->nodes[k]->weights[j] * output_layer->nodes[j]->delta_value;
@@ -68,12 +72,12 @@ void _nn_backPropagation(nn_Model* model, double* desired_output)
   /* ATTENTION: next_layer is the next layer looking at the model BACKWARDS.
    * So if current_layer is the layer at index 'i', next_layer is at the index 'i - 1' (towards the input layer)
    */
-  for (size_t i = model_layers->num_hidden_layers - 1; /* manually breaking btw */; i--) { // for (...yers - 1 ; i-- > 0; )
+  for (size_t i = model_layers->num_hidden_layers - 1; /* manually breaking later instead of condition here */ ; i--) {
     current_layer = &model_layers->hidden_layers[i];
     next_layer = i == 0 ? &model_layers->input_layer : &model_layers->hidden_layers[i - 1];
     // calculate derivatives for current layer
     for (size_t j = 0; j < current_layer->nb_nodes; j++) {
-      current_layer->nodes[j]->delta_value = current_layer->nodes[j]->d_value * _nn_derivativeActivation(current_layer->nodes[j]->raw_value, current_layer->activation);
+      current_layer->nodes[j]->delta_value = current_layer->nodes[j]->d_value * _nn_derivativeActivation(current_layer->nodes[j]->value, current_layer->activation);
       // calculate derivatives for next layer
       for (size_t k = 0; k < next_layer->nb_nodes; k++) {
         next_layer->nodes[k]->d_weights[j] = current_layer->nodes[j]->delta_value * next_layer->nodes[k]->value;
@@ -84,7 +88,7 @@ void _nn_backPropagation(nn_Model* model, double* desired_output)
       current_layer->nodes[j]->d_bias = current_layer->nodes[j]->delta_value;
     }
     if (i == 0) {
-      break; // cannot check if i <= 0 bc i has type size_t
+      break; // cannot check if i <= 0 in for loop bc i has type size_t (unsigned)
     }
   }
 }
