4510353bc55ec3e7a96524725cf92216181ebd32
Nicolas Reyland <nicolas.reyland@gmail.com>
2021-10-23 13:38:53 +0200

    extra/test.c now loading the mnist/\*-19k.{in,ou} db

--- extra/test.c
+++ extra/test.c
@@ -18,26 +18,30 @@ int main(int argc, char** argv)
 	initMemoryTracking();
 
 	// model architecture
-	nn_ShapeDescription model_architecture[3] = {
-		create1DShapeDescription(2), // create1DShapeDescription(2),
-		create1DShapeDescription(2), // create1DShapeDescription(2),
-		create1DShapeDescription(1), // { .dims = 1, .x = 1, .y = 1, .z = 1 }
+	nn_ShapeDescription model_architecture[4] = {
+		create2DShapeDescription(28, 28),
+		create2DShapeDescription(14, 14),
+		create1DShapeDescription(128),
+		create1DShapeDescription(9),
 	};
 	// activation functions
-	activation activations[2] = {
-		SIGMOID, SOFTMAX
+	activation activations[3] = {
+		RELU, RELU, SOFTMAX
 	};
 	// loss & optimizers
 	losses loss = CATEGORICALCROSSENTROPY;
 	optimizer optimizer = ADAM;
 
 	verbose("Allocating model...");
-
 	// malloc model
 	nn_Model* model = createModel(3, model_architecture, activations, loss, optimizer);
+	verbose("Model allocated");
 	nn_ShapeDescription shape = emptyShapeDescription();
-	nn_Data* data_train = nn_DataLoadRaw("datas/xor/input.txt", "datas/xor/output.txt", &shape);
-	nn_Data* data_test = nn_DataLoadRaw("datas/xor/input.txt", "datas/xor/output.txt", &shape);
+	verbose("Reading the training data ...");
+	nn_Data* data_train = nn_DataLoadRaw("datas/mnist/train-first-19000.in", "datas/mnist/train-first-19000.out", &shape, true);
+	verbose("Reading the testing data ...");
+	nn_Data* data_test = nn_DataLoadRaw("datas/mnist/test.in", "datas/mnist/test.out", &shape, true);
+	verbose("Data read.");
 	nn_DataSet* dataset = _nn_createDataSet(data_train, data_test);
 	nn_Session* session = createSession(
 		dataset,
@@ -49,15 +53,15 @@ int main(int argc, char** argv)
 	);
 
 	verbose("Session allocated");
-	setVerbose(false);
 	session->train(session, model);
-	setVerbose(true);
 	session->test(session, model);
 
 	verbose("Model allocated.");
 
 	// free model
 	freeModel(model);
+	// free session
+	freeSession(session);
 
 	verbose("Model freed.");
 
