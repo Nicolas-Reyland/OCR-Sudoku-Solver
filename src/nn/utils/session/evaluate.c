21d66b15163330b7a032c7e2952bedee27876bd4
Nicolas Reyland <nicolas@reyland.dev>
2021-11-19 17:55:35 +0100

    prepared implementation of layer-based derivative functions

--- src/nn/utils/session/evaluate.c
+++ src/nn/utils/session/evaluate.c
@@ -27,41 +27,43 @@ void _nn_feedForward(nn_Model* model, double* input)
 
 void _nn_backPropagation(nn_Model* model, double* desired_output)
 {
-  // https://machinelearningmastery.com/implement-backpropagation-algorithm-scratch-python/
-  // TODO: update comments
+    // https://machinelearningmastery.com/implement-backpropagation-algorithm-scratch-python/
+    // TODO: update comments
 
-  // get model layers struct
-  nn_Layer** const layers = model->layers;
-  size_t num_layers = model->num_layers;
+    // get model layers struct
+    nn_Layer** const layers = model->layers;
+    size_t num_layers = model->num_layers;
 
-  // output layer error rates
-  for (size_t j = 0; j < layers[num_layers - 1]->num_nodes; j++) {
-    //
-    double d_error = layers[num_layers-1]->nodes[j]->value - desired_output[j];
-    layers[num_layers-1]->nodes[j]->d_raw_value = d_error * _nn_derivativeActivation(layers[num_layers-1]->nodes[j]->raw_value, layers[num_layers-1]->activation);
-    //
-    for (size_t k = 0; k < layers[num_layers-2]->num_nodes; k++) {
-      layers[num_layers-2]->nodes[k]->d_weights[j] = layers[num_layers-1]->nodes[j]->d_raw_value * layers[num_layers-2]->nodes[k]->value;
-      layers[num_layers-2]->nodes[k]->d_value = layers[num_layers-2]->nodes[k]->weights[j] * layers[num_layers-1]->nodes[j]->d_raw_value;
+    // output layer error rates
+    _nn_derivativeLayerActivation(layers[num_layers - 1]);
+    for (size_t j = 0; j < layers[num_layers - 1]->num_nodes; j++) {
+        //
+        double d_error = layers[num_layers-1]->nodes[j]->value - desired_output[j];
+        layers[num_layers-1]->nodes[j]->d_raw_value *= d_error;// * _nn_derivativeActivation(layers[num_layers-1]->nodes[j]->raw_value, layers[num_layers-1]->activation);
+        //
+        for (size_t k = 0; k < layers[num_layers-2]->num_nodes; k++) {
+            layers[num_layers-2]->nodes[k]->d_weights[j] = layers[num_layers-1]->nodes[j]->d_raw_value * layers[num_layers-2]->nodes[k]->value;
+            layers[num_layers-2]->nodes[k]->d_value = layers[num_layers-2]->nodes[k]->weights[j] * layers[num_layers-1]->nodes[j]->d_raw_value;
+        }
+
+        layers[num_layers-1]->nodes[j]->d_bias = layers[num_layers-1]->nodes[j]->d_raw_value;
     }
 
-    layers[num_layers-1]->nodes[j]->d_bias = layers[num_layers-1]->nodes[j]->d_raw_value;
-  }
+    // hidden & input layers
+    for (size_t i = num_layers - 2; i > 0; i--) {
+        _nn_derivativeLayerActivation(layers[i]);
+        for (size_t j = 0; j < layers[i]->num_nodes; j++) {
+        layers[i]->nodes[j]->d_raw_value *= layers[i]->nodes[j]->d_value;// * _nn_derivativeActivation(layers[i]->nodes[j]->raw_value, layers[i]->activation);
 
-  // hidden & input layers
-  for (size_t i = num_layers - 2; i > 0; i--) {
-    for (size_t j = 0; j < layers[i]->num_nodes; j++) {
-      layers[i]->nodes[j]->d_raw_value = layers[i]->nodes[j]->d_value * _nn_derivativeActivation(layers[i]->nodes[j]->raw_value, layers[i]->activation);
+        for (size_t k = 0; k < layers[i-1]->num_nodes; k++) {
+            layers[i-1]->nodes[k]->d_weights[j] = layers[i]->nodes[j]->d_raw_value * layers[i-1]->nodes[k]->value;
+            if (i > 1)
+                layers[i-1]->nodes[k]->d_value = layers[i-1]->nodes[k]->weights[j] * layers[i]->nodes[j]->d_raw_value;
+        }
 
-      for (size_t k = 0; k < layers[i-1]->num_nodes; k++) {
-        layers[i-1]->nodes[k]->d_weights[j] = layers[i]->nodes[j]->d_raw_value * layers[i-1]->nodes[k]->value;
-        if (i > 1)
-          layers[i-1]->nodes[k]->d_value = layers[i-1]->nodes[k]->weights[j] * layers[i]->nodes[j]->d_raw_value;
-      }
-
-      layers[i]->nodes[j]->d_bias = layers[i]->nodes[j]->d_raw_value;
+        layers[i]->nodes[j]->d_bias = layers[i]->nodes[j]->d_raw_value;
+        }
     }
-  }
 }
 
 void _nn_updateWeights(nn_Model* model, double learning_rate)
--- src/nn/utils/session/functions/derivation.c
+++ src/nn/utils/session/functions/derivation.c
@@ -6,10 +6,48 @@ double dSigmoid(double x);
 double dRelu(double x);
 double dLeakyRelu(double x);
 double dtan_h(double x);
+void dSoftmax(nn_Layer* layer);
+
+void _nn_derivativeLayerActivation(nn_Layer* layer)
+{
+    //double (monoid*)(double);
+    switch(layer->activation)
+    {
+        case SIGMOID:
+            for (size_t i = 0; i < layer->num_nodes; i++)
+                layer->nodes[i]->d_raw_value = dSigmoid(layer->nodes[i]->raw_value);
+            break;
+        case RELU:
+            for (size_t i = 0; i < layer->num_nodes; i++)
+                layer->nodes[i]->d_raw_value = dRelu(layer->nodes[i]->raw_value);
+            break;
+        case LEAKY_RELU:
+            for (size_t i = 0; i < layer->num_nodes; i++)
+                layer->nodes[i]->d_raw_value = dLeakyRelu(layer->nodes[i]->raw_value);
+            break;
+        case TANH:
+            for (size_t i = 0; i < layer->num_nodes; i++)
+                layer->nodes[i]->d_raw_value = dtan_h(layer->nodes[i]->raw_value);
+            break;
+        case SOFTMAX:
+            // difficult case
+            dSoftmax(layer);
+            break;
+        default:
+            fprintf(stderr,
+                "derivativeActivation: Unrecognised activation function: %s\n",
+                activation_str[layer->activation]);
+                exit(EXIT_FAILURE);
+            break;
+    }
+}
 
 /* Derivative Activation */
 double _nn_derivativeActivation(double x, activation activation)
 {
+    return 1.0;
+
+    // TODO: remove this function
     double output;
     switch(activation)
     {
@@ -22,17 +60,22 @@ double _nn_derivativeActivation(double x, activation activation)
         case TANH:
             return dtan_h(x);
         case SOFTMAX:
-            // there is no derivative for softmax
             // returning 1.0 so it has no impact (x * derivative = x * 1.0 = x)
             return 1.0;
         default:
-        fprintf(stderr,
-            "derivativeActivation: Unrecognised activation function: %d\n",
-            activation);
-            exit(EXIT_FAILURE);
-        break;
+            fprintf(stderr,
+                "derivativeActivation: Unrecognised activation function: %d\n",
+                activation);
+                exit(EXIT_FAILURE);
+            break;
     }
     return output;
+}
+
+void dSoftmax(nn_Layer* layer)
+{
+    for (size_t i = 0; i < layer->num_nodes; i++)
+        layer->nodes[i]->d_raw_value = 1.0;
 }
 
 double dSigmoid(double x)
