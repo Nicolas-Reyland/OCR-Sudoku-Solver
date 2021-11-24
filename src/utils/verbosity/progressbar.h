6b8517dc12d424717351ab36527716cf26fea422
Nicolas Reyland <nicolas@reyland.dev>
2021-11-24 21:57:40 +0100

    implemented a generic progressbar in verbosity

---
+++ src/utils/verbosity/progressbar.h
@@ -0,0 +1,28 @@
+// progressbar.h
+
+#ifndef UTILS_VERBOSITY_PROGRESSBAR_H
+#define UTILS_VERBOSITY_PROGRESSBAR_H
+
+#include "verbose.h"
+
+#define PROGRESSBAR_PASSED '='
+#define PROGRESSBAR_REMAIN '-'
+
+typedef struct ProgressBar ProgressBar;
+struct ProgressBar
+{
+    const char* name;
+    size_t start;
+    size_t end;
+    size_t range;
+    size_t state;
+    size_t width;
+    size_t drawn;
+    float percentage;
+};
+
+ProgressBar createProgressBar(const char* name, size_t start, size_t end, size_t width);
+void updateProgressBar(ProgressBar* bar, size_t state);
+void endProgressBar(ProgressBar* bar);
+
+#endif
