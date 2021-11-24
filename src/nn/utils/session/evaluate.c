43518982451db09454ea1966a9e5decd8671a080
Nicolas Reyland <nicolas@reyland.dev>
2021-11-24 18:52:52 +0100

    fixed typo

--- src/nn/utils/session/evaluate.c
+++ src/nn/utils/session/evaluate.c
@@ -54,7 +54,7 @@ void _nn_backPropagation(nn_Model* model, double* desired_output)
         _nn_derivativeLayerActivation(layers[i]);
         for (size_t j = 0; j < layers[i]->num_nodes; j++) {
             if (isnan(layers[i]->nodes[j]->d_raw_value)) {
-                verbose("r_raw_value is nan");
+                verbose("d_raw_value is nan");
                 exit(EXIT_FAILURE);}
             layers[i]->nodes[j]->d_raw_value *= layers[i]->nodes[j]->d_value;// * _nn_derivativeActivation(layers[i]->nodes[j]->raw_value, layers[i]->activation);
 
