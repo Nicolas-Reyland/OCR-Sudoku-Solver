b977e77709aee1bfb102d3312214522c74f6c32d
Nicolas Reyland <nicolas@reyland.dev>
2021-11-27 13:35:30 +0100

    implemented implicit logger disabling

--- src/utils/misc/logging.c
+++ src/utils/misc/logging.c
@@ -6,18 +6,23 @@ bool LOGGING_ENABLED = true;
 
 Logger createLogger(const char* file_path)
 {
+    if (file_path == NULL)
+        goto noLogFile;
     FILE* fptr = fopen(file_path, "w");
     if (fptr == NULL) {
-        err_verbose("Logging file could not be opened. Disabling this logger");
-        return (Logger) {
-            .file_path = NULL,
-            .fptr = NULL,
-            .num_logs = 0,
-        };
+        err_verbose("Logging file could not be opened. This logger is disabled");
+        goto noLogFile;
     }
     return (Logger) {
         .file_path = file_path,
         .fptr = fptr,
+        .num_logs = 0,
+    };
+
+    noLogFile:
+    return (Logger) {
+        .file_path = NULL,
+        .fptr = NULL,
         .num_logs = 0,
     };
 }
